facb54af596b41ffbdbf7a937f2743b39d4d7378
Guilherme Torres <automacoes@lumma.com.br>
2023-12-07 11:50:46 -0300

    skip model

--- main.js
+++ main.js
@@ -15,9 +15,21 @@ async function runAutomation() {
 
     const page = await browser.newPage();
 
-    await page.goto(linkECRV)
+    await page.goto(linkECRV, { waitUntil: 'networkidle2' })
 
+    const frameBody = page.frames().find(frame => frame.name() === 'body');
 
+    if (frameBody) {
+
+        await frameBody.waitForSelector('.modal-footer .btn-primary', { visible: true });
+        await frameBody.waitForSelector('#notificacoes > div > div > div.modal-footer > div > div.col-sm-4 > button')
+        await frameBody.evaluate(() => {
+            document.querySelector('.modal-footer .btn-primary').click();
+        });
+
+    } else {
+        console.log("Frame 'body' não encontrado.");
+    }
 
     // await browser.close()
 
