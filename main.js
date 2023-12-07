cd9e95730e2a9b7bc202c0fc09ae0bc8ed179f47
Guilherme Torres <automacoes@lumma.com.br>
2023-12-07 15:51:55 -0300

    login to ecrvsp

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const Delay = require('./modules/Pup_modules/delay');
 
 async function runAutomation() {
 
+    const CPF = process.env.CPF
+    const SENHA = process.env.SENHA
     const linkECRV = 'https://www.e-crvsp.sp.gov.br/'
 
     const browser = await puppeteer.launch({
@@ -26,13 +28,30 @@ async function runAutomation() {
         await frameBody.evaluate(() => {
             document.querySelector('.modal-footer .btn-primary').click();
         });
+        await Delay(4000)
+        await frameBody.waitForSelector('#cpf')
+        await frameBody.type('#cpf', CPF)
+        await frameBody.waitForSelector('#senha')
+        await frameBody.type('#senha', SENHA)
 
-    } else {
-        console.log("Frame 'body' não encontrado.");
+        page.on('dialog', async dialog => {
+            await dialog.dismiss();
+        });
+        await frameBody.evaluate(() => {
+            document.querySelector('#conteudo > div.container.container-home > div > div.col-sm-3 > div > div.panel-body > button').click();
+        });
+        await Delay(2000)
+        await frameBody.waitForSelector('#notificacoes > div > div > div.modal-footer > div > div.col-sm-4 > button')
+        await frameBody.evaluate(() => {
+            document.querySelector('.modal-footer .btn-primary').click();
+        });
+
+        await Tab(page, 13)
+        await page.keyboard.press('Enter')
+
+        // await browser.close()
+
     }
-
-    // await browser.close()
-
 }
 
-runAutomation()
+runAutomation();
